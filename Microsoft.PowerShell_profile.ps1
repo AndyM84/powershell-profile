847d4db7ce99892f026a7ea79a71fedf8629736b
Andrew Male <amale@zibings.com>
2014-03-07 21:10:30 -0500

    Adding scripts

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+﻿$host.UI.RawUI.BackgroundColor = “Black”;
+Clear-Host
+
+# Load posh-git example profile
+. 'D:\Personal\posh-git\profile.example.ps1'
+
+Get-ChildItem '~\Documents\WindowsPowerShell\Profile\Scripts' |
+Where { $_.Name -notlike '__*' -and $_.Name -like '*.ps1'} |
+ForEach { . $_.FullName }
+
+D:
+cd \
