8ef8e2dd5d1cd81e0beb61cf60327a28bdb929a4
Andrew Male <amale@zibings.com>
2014-03-12 23:24:28 -0400

    Forgot to fix path for scripts directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@ Clear-Host
 # Load posh-git example profile
 . 'D:\Personal\posh-git\profile.example.ps1'
 
-Get-ChildItem '~\Documents\WindowsPowerShell\Profile\Scripts' |
+Get-ChildItem '~\Documents\WindowsPowerShell\Scripts' |
 Where { $_.Name -notlike '__*' -and $_.Name -like '*.ps1'} |
 ForEach { . $_.FullName }
 
